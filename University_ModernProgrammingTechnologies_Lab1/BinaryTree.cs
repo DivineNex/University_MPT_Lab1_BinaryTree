b8c8dd786e61837e93c0f910dd28d71fc4bd94ed
Nikolay Makalov <nexterrible@mail.ru>
2022-09-18 15:59:20 +0700

    BinaryTree class v0.1

---
+++ University_ModernProgrammingTechnologies_Lab1/BinaryTree.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace University_ModernProgrammingTechnologies_Lab1
+{
+    internal class BinaryTree
+    {
+        public BinaryTree()
+        {
+
+        }
+
+        public void Insert()
+        {
+
+        }
+
+        public void Remove()
+        {
+
+        }
+
+        public void Search()
+        {
+
+        }
+
+        public void Display()
+        {
+
+        }
+
+        public void Recount()
+        {
+
+        }
+
+        public void Clear()
+        {
+
+        }
+
+        public bool IsEmpty()
+        {
+            return true;
+        }
+    }
+}
