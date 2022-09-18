72e4155b10e3a70dc6be24b884af852a1e082a7b
Nikolay Makalov <nexterrible@mail.ru>
2022-09-18 15:48:13 +0700

    GUI v0.1

--- University_ModernProgrammingTechnologies_Lab1/Program.cs
+++ University_ModernProgrammingTechnologies_Lab1/Program.cs
@@ -16,7 +16,7 @@ namespace University_ModernProgrammingTechnologies_Lab1
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new formMain());
         }
     }
 }
