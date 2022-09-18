b92a502f0249b443c4bae28e18b8ecebed178a47
Nikolay Makalov <nexterrible@mail.ru>
2022-09-18 16:42:08 +0700

    BinaryTree class v0.2 (currently only for Bearings)
    Минимальная реализация добавления подшипника по динамической грузоподъемности. При этом подшипники, разные по другим характеристикам, но одинаковые по дин. грузоподъемности, пока добавляться не будут

--- University_ModernProgrammingTechnologies_Lab1/BinaryTree.cs
+++ University_ModernProgrammingTechnologies_Lab1/BinaryTree.cs
@@ -3,19 +3,54 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace University_ModernProgrammingTechnologies_Lab1
 {
     internal class BinaryTree
     {
+        private const string ELEMENT_EXIST_MESSAGE = "The element has already in the tree";
+        private BinaryTreeItem _rootBearing;
+
         public BinaryTree()
         {
-
+            _rootBearing = null;
         }
 
-        public void Insert()
+        public void Insert(RadialBearing radialBearing)
         {
+            BinaryTreeItem binaryTreeItem = new BinaryTreeItem(radialBearing);
 
+            if (_rootBearing == null)
+            {
+                _rootBearing = binaryTreeItem;
+                return;
+            }
+
+            InsertItem(_rootBearing, binaryTreeItem);
+        }
+
+        private void InsertItem(BinaryTreeItem currentItem, BinaryTreeItem newItem)
+        {
+            if (newItem.Bearing.C < currentItem.Bearing.C)
+            {
+                if (currentItem.LeftItem == null)
+                    currentItem.LeftItem = newItem;
+                else
+                    InsertItem(currentItem.LeftItem, newItem);
+            }
+            else if (newItem.Bearing.C > currentItem.Bearing.C)
+            {
+                if (currentItem.RightItem == null)
+                    currentItem.RightItem = newItem;
+                else
+                    InsertItem(currentItem.RightItem, newItem);
+            }
+            else
+            {
+                MessageBox.Show(ELEMENT_EXIST_MESSAGE);
+            }
+            
         }
 
         public void Remove()
---
+++ University_ModernProgrammingTechnologies_Lab1/BinaryTreeItem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace University_ModernProgrammingTechnologies_Lab1
+{
+    internal class BinaryTreeItem
+    {
+        public RadialBearing Bearing { get; private set; }
+        public BinaryTreeItem LeftItem { get; set; }
+        public BinaryTreeItem RightItem { get; set; }
+
+        public BinaryTreeItem(RadialBearing radialBearing)
+        {
+            Bearing = radialBearing;
+            LeftItem = null;
+            RightItem = null;
+        }
+    }
+}
