45236e703768672622837947ee86802521604883
Nikolay Makalov <nexterrible@mail.ru>
2022-09-22 01:21:47 +0700

    Some binary tree architecture refactoring
    Поменял немного архитектуру бинарного дерева. Теперь это будет абстрактный класс с абстрактными членами, от которого я уже буду наследовать бинарное дерево конкретно для радиальных подшипников

--- University_ModernProgrammingTechnologies_Lab1/BinaryTree.cs
+++ University_ModernProgrammingTechnologies_Lab1/BinaryTree.cs
@@ -7,80 +7,28 @@ using System.Windows.Forms;
 
 namespace University_ModernProgrammingTechnologies_Lab1
 {
-    internal class BinaryTree
+    internal abstract class BinaryTree
     {
-        private const string ELEMENT_EXIST_MESSAGE = "The element has already in the tree";
-        private BinaryTreeItem _rootBearing;
+        private protected const string ELEMENT_EXIST_MESSAGE = "The element has already in the tree";
+        public abstract BinaryTreeItem _root { get; set; }
 
         public BinaryTree()
         {
-            _rootBearing = null;
+            _root = null;
         }
 
-        public void Insert(RadialBearing radialBearing)
-        {
-            BinaryTreeItem binaryTreeItem = new BinaryTreeItem(radialBearing);
+        public abstract void Insert();
 
-            if (_rootBearing == null)
-            {
-                _rootBearing = binaryTreeItem;
-                return;
-            }
+        private protected abstract void _Insert();
 
-            _Insert(_rootBearing, binaryTreeItem);
-        }
+        public abstract void Remove();
 
-        private void _Insert(BinaryTreeItem currentItem, BinaryTreeItem newItem)
-        {
-            if (newItem.Bearing.C < currentItem.Bearing.C)
-            {
-                if (currentItem.LeftItem == null)
-                    currentItem.LeftItem = newItem;
-                else
-                    _Insert(currentItem.LeftItem, newItem);
-            }
-            else if (newItem.Bearing.C > currentItem.Bearing.C)
-            {
-                if (currentItem.RightItem == null)
-                    currentItem.RightItem = newItem;
-                else
-                    _Insert(currentItem.RightItem, newItem);
-            }
-            else
-            {
-                MessageBox.Show(ELEMENT_EXIST_MESSAGE);
-            }
-            
-        }
+        public abstract void Search();
 
-        public void Remove()
-        {
+        public abstract void Recount();
 
-        }
+        public abstract void Clear();
 
-        public void Search()
-        {
-
-        }
-
-        public void Display()
-        {
-
-        }
-
-        public void Recount()
-        {
-
-        }
-
-        public void Clear()
-        {
-
-        }
-
-        public bool IsEmpty()
-        {
-            return true;
-        }
+        public abstract bool IsEmpty();
     }
 }
---
+++ University_ModernProgrammingTechnologies_Lab1/RadialBearingBinaryTree.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace University_ModernProgrammingTechnologies_Lab1
+{
+    internal sealed class RadialBearingBinaryTree : BinaryTree
+    {
+        public override BinaryTreeItem _root { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public override void Clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Insert()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool IsEmpty()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Recount()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Remove()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Search()
+        {
+            throw new NotImplementedException();
+        }
+
+        private protected override void _Insert()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
